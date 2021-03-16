205c50e8cf87e00e094f285f33cdc2658cd73857
Merykson-a <79925744+Merykson-a@users.noreply.github.com>
2021-03-16 17:53:08 -0300

    Corrigindo bugs

--- src/main/java/br/com/devmedia/introspringbootweb/repository/AluguelRepository.java
+++ src/main/java/br/com/devmedia/introspringbootweb/repository/AluguelRepository.java
@@ -12,10 +12,10 @@ public interface AluguelRepository extends JpaRepository<Aluguel, Long> {
     /*@Query("select m from Aluguel m where m.usuario.id = ?1 and (m.livro.nome like %?2% or m.usuario.nome like %?2%)")
     Page<Aluguel> findById(long usuarioId, String pesquisa, Pageable pageable);*/
 
-    @Query(value = "SELECT a FROM Aluguel a WHERE a.usuario.id = ?1 and (to_char(a.dataAluguel, '%d/%m/%Y') like CONCAT('%' , ?2 , '%') or" +
-            " to_char(a.dataDevolucao, '%d/%m/%Y') like CONCAT('%' , ?2 , '%') or" +
-            " to_char(a.prevDataDevolucao, '%d/%m/%Y') like CONCAT('%' , ?2 , '%') or" +
-            " a.livro.nome like CONCAT('%' , ?2 , '%') or a.usuario.nome like CONCAT('%' , ?2 , '%'))")
+    @Query(value = "SELECT a FROM Aluguel a WHERE a.usuario.id = ?1 and (to_char(a.dataAluguel, 'dd/mm/yyyy') ilike CONCAT('%' , ?2 , '%') or" +
+            " to_char(a.dataDevolucao, 'dd/mm/yyyy') ilike CONCAT('%' , ?2 , '%') or" +
+            " to_char(a.prevDataDevolucao, 'dd/mm/yyyy') ilike CONCAT('%' , ?2 , '%') or" +
+            " a.livro.nome like CONCAT('%' , ?2 , '%') or a.usuario.nome ilike CONCAT('%' , ?2 , '%'))")
     Page<Aluguel> findById(long usuarioId, String pesquisa, Pageable pageable);
 
 }
