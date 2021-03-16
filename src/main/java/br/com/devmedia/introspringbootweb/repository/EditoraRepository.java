52474549791b52c95d1ed6635fcd617cae0d00fe
Merykson-a <79925744+Merykson-a@users.noreply.github.com>
2021-03-16 20:20:56 -0300

    Corrigindo bugs

--- src/main/java/br/com/devmedia/introspringbootweb/repository/EditoraRepository.java
+++ src/main/java/br/com/devmedia/introspringbootweb/repository/EditoraRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 @Repository
 public interface  EditoraRepository extends JpaRepository<Editora, Long> {
 
-    @Query("select m from Editora m where to_char(a.id, 'FM999999999999999999') like %?1% or lower(m.nome) like lower(CONCAT('%' , ?1 , '%')) or lower(m.cidade) like lower(CONCAT('%' , ?1 , '%'))")
+    @Query("select m from Editora m where lower(m.nome) like lower(CONCAT('%' , ?1 , '%')) or lower(m.cidade) like lower(CONCAT('%' , ?1 , '%'))")
     Page<Editora> findByTodos(String pesquisa, Pageable pageable);
 }
