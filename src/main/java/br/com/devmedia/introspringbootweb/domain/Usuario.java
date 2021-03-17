863875d0b3d147976e94df728191a6f04bb42853
Merykson-a <79925744+Merykson-a@users.noreply.github.com>
2021-03-17 14:05:33 -0300

    Debugs

--- src/main/java/br/com/devmedia/introspringbootweb/domain/Usuario.java
+++ src/main/java/br/com/devmedia/introspringbootweb/domain/Usuario.java
@@ -4,6 +4,7 @@ package br.com.devmedia.introspringbootweb.domain;
 import br.com.devmedia.introspringbootweb.domain.Aluguel;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
@@ -29,6 +30,7 @@ public class Usuario{
 
     @NotBlank(message = "O campo email não pode ser vazio")
     @Column(nullable = false)
+    @Email
     private String email;
 
     @OneToMany
