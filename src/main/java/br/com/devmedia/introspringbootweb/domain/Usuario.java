4a1888923c0abd7d61ecdae119830b2742492f3b
Merykson-a <79925744+Merykson-a@users.noreply.github.com>
2021-03-26 12:23:54 -0300

    Corrigindo validações

--- src/main/java/br/com/devmedia/introspringbootweb/domain/Usuario.java
+++ src/main/java/br/com/devmedia/introspringbootweb/domain/Usuario.java
@@ -20,6 +20,8 @@ public class Usuario{
 
 
     @Column(nullable = false, length = 60)
+/*    @NotBlank(message = "O campo nome não pode ser vazio!")
+    @Size(min = 3, max = 60, message = "No mínimo 3 caracteres e no máximo 60!")*/
     @Pattern(regexp="[a-zA-Z á-úÀ-Ú]{3,60}", message = "Insira um nome válido, sem caracteres especiais!")
     private String nome;
 
@@ -31,8 +33,9 @@ public class Usuario{
 
 
     @Column(nullable = false)
-    @NotBlank(message = "O campo cidade não pode ser vazio!")
-    @Size(min = 3, max = 60, message = "No mínimo 3 caracteres e no máximo 60!")
+    /*@NotBlank(message = "O campo cidade não pode ser vazio!")
+    @Size(min = 3, max = 60, message = "No mínimo 3 caracteres e no máximo 60!")*/
+    @Pattern(regexp="[a-zA-Z á-úÀ-Ú]{3,60}", message = "Insira uma cidade válida, sem caracteres especiais!")
     private String cidade;
 
 
