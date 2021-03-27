7bdc218599e2937d4c62a6843c62a8365c7da3c1
Merykson-a <79925744+Merykson-a@users.noreply.github.com>
2021-03-26 21:53:49 -0300

    Debugs

---
+++ src/main/java/br/com/devmedia/introspringbootweb/controller/HomeController.java
@@ -0,0 +1,20 @@
+package br.com.devmedia.introspringbootweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/teste")
+public class HomeController {
+
+    @GetMapping("/listar")
+    public ModelAndView listar(ModelMap model) {
+        return new ModelAndView("teste/teste", model);
+    }
+
+}
